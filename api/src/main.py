e469ac51aa61d2b77b147cb55d375e3ccadb6a11
ethank007@hotmail.com <ethank007@hotmail.com>
2025-08-14 16:10:15 -0400

    Overwriting fuck up

---
+++ api/src/main.py
@@ -0,0 +1,6 @@
+import from utils.similarity_utils import similarity_search
+
+def main():
+    # Example usage of the similarity_search function
+    result = similarity_search()
+    print("Similarity search result:", result)
